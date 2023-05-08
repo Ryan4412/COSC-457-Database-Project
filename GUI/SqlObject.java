c438bf8f9d860e81356b1fb363c82f13b9236e1d
bseabol1 <99768961+bseabol1@users.noreply.github.com>
2023-05-07 20:28:00 -0400

    added error handling

--- GUI/SqlObject.java
+++ GUI/SqlObject.java
@@ -1,6 +1,7 @@
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JLabel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -52,29 +53,46 @@ public class SqlObject {
             }
             displayTable(print);
         } catch (SQLException e) {
-            System.err.println(e);
+            JLabel prompt = new JLabel();
+            prompt.setText("The insert or delete was unsuccessful, please try again.");
+            
+            main.removeAll();
+            main.add(prompt);
+            main.revalidate();
+            main.repaint();
         }
     }
 
     public void displayTable(ArrayList<ArrayList> print) {
-        String[][] row = new String[print.size()][print.get(0).size()];
-        for (int i = 0; i < print.size(); i++) {
-            for (int j = 0; j < print.get(0).size(); j++) {
-                row[i][j] = (String) print.get(i).get(j);
+        if (print.size() != 0) {
+            String[][] row = new String[print.size()][print.get(0).size()];
+            for (int i = 0; i < print.size(); i++) {
+                for (int j = 0; j < print.get(0).size(); j++) {
+                    row[i][j] = (String) print.get(i).get(j);
+                }
             }
-        }
-        JTable jt = new JTable(row, column);
-        jt.setBounds(0, 0, 200, 1000);
-        // JScrollPane sp = new JScrollPane(jt);
-        // added
-        JScrollPane sp = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            JTable jt = new JTable(row, column);
+            jt.setBounds(0, 0, 200, 1000);
+            // JScrollPane sp = new JScrollPane(jt);
+            // added
+            JScrollPane sp = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                    JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
-        main.removeAll();
-        main.add(sp);
-        main.revalidate();
-        main.repaint();
+            main.removeAll();
+            main.add(sp);
+            main.revalidate();
+            main.repaint();
+        }
+        else {
+            JLabel prompt = new JLabel();
+            prompt.setText("The update, insert, or query was unsuccessful, please try again.");
+            
+            main.removeAll();
+            main.add(prompt);
+            main.revalidate();
+            main.repaint();
+        }
 
     }
 
@@ -97,7 +115,13 @@ public class SqlObject {
             }
             displayTable(print);
         } catch (SQLException e) {
-            System.err.println(e);
+            JLabel prompt = new JLabel();
+            prompt.setText("The query was unsuccessful, please try again.");
+            
+            main.removeAll();
+            main.add(prompt);
+            main.revalidate();
+            main.repaint();
         }
     }
 
